a65796f3cfa75325f939a4569d9e3f9d251a53c5
CoolKiid95 <diegoal.delgadoc@gmail.com>
2024-12-11 20:54:46 -0500

    components created

---
+++ src/app/components/aboutusnav/aboutusnav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aboutusnav',
+  standalone: true,
+  imports: [],
+  templateUrl: './aboutusnav.component.html',
+  styleUrl: './aboutusnav.component.css'
+})
+export class AboutusnavComponent {
+
+}
---
+++ src/app/components/nav-products/nav-products.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-products.component.html',
+  styleUrl: './nav-products.component.css'
+})
+export class NavProductsComponent {
+
+}
