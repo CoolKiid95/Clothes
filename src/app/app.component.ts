869cf96a52ccb8f6692471e10fb8423a0a1f20d3
Juan Santiago Fajardo Aldana <rallalpiso3@gmail.com>
2024-12-13 21:14:58 -0500

    productdisplay basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductDisplayComponent } from './components/product-display/product-display.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavProductsComponent } from "./components/nav-products/nav-products.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductDisplayComponent, FooterComponent, NavProductsComponent, NavProductsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'clothesproject';
+  
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProductDisplayComponent } from './components/product-display/product-display.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"displayproduct", component:ProductDisplayComponent},
+    {path:"nav", component:NavbarComponent}
+    
+];
