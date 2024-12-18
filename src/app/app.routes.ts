99c86f8337dc5045bb1624703a4adfd5eb6c9d8c
lina964 <linamariaalvaradososa@gmail.com>
2024-12-18 18:36:46 -0500

    tienda y carrito

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,6 +11,7 @@ import { LoginComponent } from './components/public/login/login.component';
 import { RegisterComponent } from './components/public/register/register.component';
 import { Error404Component } from './error404/error404.component';
 import { CarritoComponent } from './components/carrito/carrito.component';
+import { ProductosComponent } from './components/productos/productos.component';
 
 
 export const routes: Routes = [
@@ -27,6 +28,7 @@ export const routes: Routes = [
     {path: "termycondiciones", component: TermsComponent},
     {path: "login", component:LoginComponent},
     {path: "register", component:RegisterComponent},
+    {path: "productos", component: ProductosComponent},
     {path: "carrito", component: CarritoComponent},
     {path: "error404", component: Error404Component},
     {path: "**", pathMatch: 'full', redirectTo: '404'}
--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -7,6 +7,8 @@ import { Injectable } from '@angular/core';
 export class ProductService {
 
   apiUrl:string ="http://localhost:4000/api"
+  token: any = sessionStorage.getItem('token')
+
   constructor(private http: HttpClient) { }
 
 
@@ -15,10 +17,13 @@ export class ProductService {
   }
   GetProductsbyCategories(categorias:string){
     console.log(categorias);
-    
-        return this.http.get(`${this.apiUrl}/productbyCategory/${categorias}`)
 
+        return this.http.get(`${this.apiUrl}/productbyCategory/${categorias}`)
   }
-  
+  addProduct (body:any ){
+    const headers = new HttpHeaders().set ("Authorization", `Bearer${this.token}`)
+    return this.http.post(`${this.apiUrl}/addproduct`, body, {headers})
+  }
+
 
 }
